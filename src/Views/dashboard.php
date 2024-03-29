23e2b04cf1b8c3288925134b8fd2599d4313badf
feras <feras.altalib@gmail.com>
2024-03-29 10:23:17 +0100

    project map

---
+++ src/Views/dashboard.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once __DIR__ . '/Includes/header.php';
+
+include_once __DIR__ . '/Includes/colonne.php';
+?>
+<div class="content">
+  <?php
+  // switch ($section) {
+  //   case '':
+  //     switch ($action) {
+  //       case '':
+  //         include_once __DIR__ . '';
+  //         break;
+
+  //       case '':
+  //         include_once __DIR__ . '';
+  //         break;
+
+  //       case '':
+  //         include_once __DIR__ . '';
+  //         break;
+
+  //       default:
+  //         include_once __DIR__ . '';
+  //         break;
+  //     }
+  //     break;
+
+  //   default:
+  //     include_once __DIR__ . '';
+  //     break;
+  // }
+  ?>
+</div>
+<?php
+include_once __DIR__ . '/Includes/footer.php';
